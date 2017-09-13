50e121c7df3227b2931cd1efa173935c54110afc
voodootuna <vdtuna@gmail.com>
2017-09-14 00:12:39 +0400

    php send mail

--- sendform.php
+++ sendform.php
@@ -5,25 +5,29 @@ use PHPMailer\PHPMailer\Exception;
 
 require_once "vendor/autoload.php"; //PHPMailer Object
 
+if($_POST){
+
 
  
 try {
 $m = new PHPMailer(true);
-//$m->isSMTP();
-//$m->SMTPAuth = true;
-//$m->STMPDebug = 2;
+$m->isSMTP();
+$m->SMTPAuth = true;
+$m->STMPDebug = 2;
 
-//$m->Host = 'smtp.gmail.com';
-//$m->Username = 'lemauricienweb@gmail.com';
-//$m->Password = 'lmweb1618';
-//$m->SMTPSecure = 'ssl';
-//$m->Port = 465;
+$m->Host = 'smtp.gmail.com';
+$m->Username = 'lemauricienweb@gmail.com';
+$m->Password = base64_decode('bG13ZWIxNjE4');
+$m->SMTPSecure = 'ssl';
+$m->Port = 465;
 
-$firstname = 'John';//$_POST['firstname'];
-$lastname = 'Doe'; //$_POST['lastname'];
-$email = 'test@lemauricien.com'; //$_POST['lemail'];
-$info = 'test info'; //$_POST['info'];
 
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$info = $_POST['info'];
+$telephone = $_POST['telephone'];
 
 
 $m->setFrom('noreply@lemauricien.com','Le Mauricien LTD', 0);
@@ -33,17 +37,38 @@ $m->addReplyTo($email, 'Reply address');
 
 //content
 $m->isHTML(true);    
-$m->Subject = 'Recrutement Journaliste Web {$lastname} {$firstname}';
-$m->Body = 'This is the HTML message body <b>in bold!</b>';
-$m->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$m->Subject = "Recrutement Journaliste Web {$lastname} {$firstname}";
+$m->Body = "<p><strong>Prenom:</strong> $firstname</p><p><strong>Nom:</strong> $lastname </p><p><strong>Tel:</strong>$telephone</p><p><strong>Email:</strong>$email</p><p><strong>Message:</strong> $info</p>";
+
+
+//attachments
+foreach($_FILES as &$file){
+  
+    if($file['name']){
+    $file_tmp = $file['tmp_name'];
+    $file_name = $file['name'];
+    $file_size = $file['size'];
+    $file_type = $file['type'];
+    echo 'uploads/'.$file_name.'<br/>';
+    move_uploaded_file($file_tmp,"uploads/".$file_name);
+    $m->addAttachment('uploads/'.$file_name);
+    }
+}
 
 
 $m->send();
  echo 'Message has been sent';
 
+ echo '<script type="text/javascript">window.location.href="./merci.html"</script>';
+
   } catch(Exception $e){
     echo 'Message could not be sent.';
     echo 'Mailer Error: ' . $m->ErrorInfo;
+
+
   } 
 
 
+}else{
+   die('GET NOT ALLOWED');
+}
